843854b5833669ff7381fbb5356c923baf89e0e1
chadhakala <85204197+chadhakala@users.noreply.github.com>
2022-09-14 14:11:30 -0400

    botched the last assignment

--- MilesPerWeek/src/MilesPerWeek.java
+++ MilesPerWeek/src/MilesPerWeek.java
@@ -9,23 +9,19 @@ public class MilesPerWeek {
 
             Scanner keyboard = new Scanner(System.in);
 
-            {
 
-                double milesPerGallon;
 
-                System.out.println("How many miles do you drive per week?");
-                int milesWeek = Integer.parseInt(keyboard.nextLine());
-
-                System.out.println("Say the following thing to your google assistant--Hey, Google. Exactly how many miles do I drive each day to and from work?");
+                System.out.println("Say the following thing to your google assistant--Hey, Google. Exactly how many miles do I drive each day to and from work? What is the answer?");
                 int dailyCommute = Integer.parseInt(keyboard.nextLine());
 
-                System.out.println("How many miles per gallon do you get per get? ");
-                milesPerGallon = keyboard.nextDouble();
+
+                int weeklyCommute = dailyCommute * 5;
+                System.out.println("How many miles per gallon do you get per get?");
+                double gallonsUsed  = Double.parseDouble(keyboard.nextLine());
 
                 double year = 260;   //work days in a year
                 double costOfGas = 4.71;  // cost per gallon
-                double costOfGaS = milesWeek * dailyCommute;    //This is a figure that represents a in vivo fuel eco
-                double total = dailyCommute * costOfGas / fuelEco * year;
+                double total = costOfGas * year * weeklyCommute / gallonsUsed;
 
                 System.out.printf("If you work 220 days out of the year you drive approx: $%1.2f%n", total);
 
