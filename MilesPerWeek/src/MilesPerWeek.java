54d146833f7aaa2d9d9a9f9c30e1e6fe9f506597
chadhakala <85204197+chadhakala@users.noreply.github.com>
2022-09-14 19:29:26 -0400

    amended for partner

--- MilesPerWeek/src/MilesPerWeek.java
+++ MilesPerWeek/src/MilesPerWeek.java
@@ -13,19 +13,21 @@ public class MilesPerWeek {
                 System.out.println("hello, my name is WESTERN-AMBER, your smart-home assistant, what's your name?");
                 String name = keyboard.nextLine();
 
-
-                System.out.println("Hi, there, "+ name +" Say the following thing to your google assistant-Hey, Google. Exactly how many miles do I drive each day to work? enter result");
-                int dailyCommute = 2 * Integer.parseInt(keyboard.nextLine());
+                System.out.println("Hello there, " + name + ", let's figure out your monthly gasoline expenses from your daily commute");
 
 
-                System.out.println("How many miles per gallon do you get per get in your typical commute of " + dailyCommute + " miles round trip?");
+                System.out.println("Say the following thing to your google assistant-Hey, Google. Exactly how many miles do I drive each day to work? enter result");
+                double dailyCommute = 2 * Double.parseDouble(keyboard.nextLine());
+
+
+                System.out.println("How many miles per gallon do you get in your typical commute of " + dailyCommute + " miles round trip?");
                 double milesPerGallon  = Double.parseDouble(keyboard.nextLine());
 
                 double month = 20;   //work days in a month
                 double costOfGas = 3.959;  // cost per gallon
                 double rawGallonsMonth = dailyCommute * month / milesPerGallon;
 
-                System.out.printf("WESTERN_AMBER says, If you work 20 days out of the month you drive approx: $%1.2f%n", rawGallonsMonth * costOfGas);
+                System.out.printf("WESTERN_AMBER says, If you work 20 days out of the month you pay approx: $%1.2f%n", rawGallonsMonth * costOfGas);
 
             }
 }
