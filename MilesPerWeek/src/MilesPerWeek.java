45e6c417f65a42a090f0c66d6def296753ac7d83
chadhakala <85204197+chadhakala@users.noreply.github.com>
2022-09-14 16:20:13 -0400

    amended for partner

--- MilesPerWeek/src/MilesPerWeek.java
+++ MilesPerWeek/src/MilesPerWeek.java
@@ -10,20 +10,22 @@ public class MilesPerWeek {
             Scanner keyboard = new Scanner(System.in);
 
 
+                System.out.println("hello, my name is WESTERN-AMBER, your smart-home assistant, what's your name?");
+                String name = keyboard.nextLine();
 
-                System.out.println("Say the following thing to your google assistant--Hey, Google. Exactly how many miles do I drive each day to and from work? What is the answer?");
-                int dailyCommute = Integer.parseInt(keyboard.nextLine());
+
+                System.out.println("Hi, there, "+ name +" Say the following thing to your google assistant-Hey, Google. Exactly how many miles do I drive each day to work? enter result");
+                int dailyCommute = 2 * Integer.parseInt(keyboard.nextLine());
 
 
-                int weeklyCommute = dailyCommute * 5;
-                System.out.println("How many miles per gallon do you get per get?");
-                double gallonsUsed  = Double.parseDouble(keyboard.nextLine());
+                System.out.println("How many miles per gallon do you get per get in your typical commute of " + dailyCommute + " miles round trip?");
+                double milesPerGallon  = Double.parseDouble(keyboard.nextLine());
 
-                double year = 260;   //work days in a year
-                double costOfGas = 4.71;  // cost per gallon
-                double total = costOfGas * year * weeklyCommute / gallonsUsed;
+                double month = 20;   //work days in a month
+                double costOfGas = 3.959;  // cost per gallon
+                double rawGallonsMonth = dailyCommute * month / milesPerGallon;
 
-                System.out.printf("If you work 220 days out of the year you drive approx: $%1.2f%n", total);
+                System.out.printf("WESTER_AMBER says If you work 20 days out of the month you drive approx: $%1.2f%n", rawGallonsMonth * costOfGas);
 
             }
 }
