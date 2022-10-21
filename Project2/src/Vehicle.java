b412eca2096aaf88123f428be2f8dc7e8b9c195f
chadhakala <85204197+chadhakala@users.noreply.github.com>
2022-10-20 23:17:54 -0400

    vehicle

---
+++ Project2/src/Vehicle.java
@@ -0,0 +1,35 @@
+public class Vehicle {
+    private String make;
+    private String color;
+    private String model;
+    private Boolean odometer;
+
+
+    public Vehicle() {                                                                                                                                                                                                                                                                  */
+        this.make = make;
+        this.color = color;
+        this.model = model;
+        this.odometer = odometer;
+    }
+
+    public boolean drive(){
+    //if (int milesDriven <= gallons || kWH){
+        //return milesDriven * gallons;
+
+    public String getMake() {
+        return make;
+    }
+    public String getColor() {
+        return make;
+    }
+    public String getModel(){
+        return model;
+    }
+    public int getOdometer() {
+        return odometer;
+    }
+    public int setOdometer(int odometer); {
+            // if (boolean drive == 1);
+
+        }
+}
