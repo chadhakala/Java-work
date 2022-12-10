3922369ecfa4e8cad6a6e042f2736d954a178179
chadhakala <85204197+chadhakala@users.noreply.github.com>
2022-12-10 12:00:21 -0500

    fetch

--- cis1500-fall2022-finalproject-chadhakala/adventureFinal/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.adventurefinal {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.adventurefinal to javafx.fxml;
-    exports com.example.adventurefinal;
-}
